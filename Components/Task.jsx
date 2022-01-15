8ccf41a25e53a0cf6c88b4655492df1f8cd65d4e
Elad Cohen <83339968+EladC921@users.noreply.github.com>
2022-01-15 18:50:04 +0200

    ontag style to stylesheet on Task.js

--- Components/Task.jsx
+++ Components/Task.jsx
@@ -3,41 +3,12 @@ import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 const Task = (props) => {
   return (
-    <View
-      style={{
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "rgba(241, 250, 238, 0.7)",
-        borderRadius: 25,
-        margin: 5,
-        alignContent: "stretch",
-      }}
-    >
-      <View style={{ flex: 1, alignItems: "flex-start", margin: 15 }}>
-        <Text
-          style={{
-            fontWeight: "500",
-            margin: 2,
-            color: "#3e577a",
-            width: 300,
-            maxWidth: "200%",
-          }}
-        >
-          {props.title}
-        </Text>
-        <Text
-          style={{
-            color: "rgb(112, 112, 112)",
-            width: 300,
-            maxWidth: "200%",
-            marginTop: 5,
-          }}
-        >
-          {props.text}
-        </Text>
+    <View style={styles.container}>
+      <View style={styles.textWrapper}>
+        <Text style={styles.title}>{props.title}</Text>
+        <Text style={styles.text}>{props.text}</Text>
       </View>
-      <View style={{ flex: 1, alignItems: "flex-end", paddingRight: "5%" }}>
+      <View style={styles.deleteWrapper}>
         <TouchableOpacity
           activeOpacity={0.5}
           onPress={() => props.deleteTask(props.id)}
@@ -50,48 +21,41 @@ const Task = (props) => {
 };
 
 const styles = StyleSheet.create({
-  item: {
-    width: "100%",
-    height: 60,
-    padding: 15,
+  container: {
     flexDirection: "row",
-    flexWrap: "wrap",
-    borderColor: "black",
-    borderBottomWidth: 1,
-  },
-
-  part: {
-    backgroundColor: "#FFF",
-    width: "45%",
-  },
-
-  inner: {
-    flex: 1,
     justifyContent: "center",
-    flexDirection: "row",
-    flexShrink: 1,
-  },
-
-  dlt: {
-    width: "10%",
-  },
-
-  dltInner: {
-    flex: 1,
     alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "rgba(241, 250, 238, 0.7)",
+    borderRadius: 25,
+    margin: 5,
+    alignContent: "stretch",
   },
 
-  itemTitle: {
+  textWrapper: {
+    flex: 1,
+    alignItems: "flex-start",
+    margin: 15,
+  },
+
+  title: {
     fontWeight: "500",
-    alignItems: "flex-start",
-    flexWrap: "wrap",
+    margin: 2,
+    color: "#3e577a",
+    width: 300,
+    maxWidth: "200%",
   },
 
-  itemText: {
+  text: {
     color: "rgb(112, 112, 112)",
-    alignItems: "flex-start",
-    flexWrap: "wrap",
+    width: 300,
+    maxWidth: "200%",
+    marginTop: 5,
+  },
+
+  deleteWrapper: {
+    flex: 1,
+    alignItems: "flex-end",
+    paddingRight: "5%",
   },
 });
 
